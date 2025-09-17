ab80c6941846289e0560160f97f6064ef1618ce5
Alexander Bush <albus87@Alexanders-MacBook-Pro.local>
2025-09-18 06:58:09 +1000

    Meeting Router built

---
+++ server/meeting_api.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const meetingRouter = express.Router();
+
+const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId, deleteAllFromDatabase } = require('./db');
+
+
+meetingRouter.get('/', (req, res, next) => {
+    const meetings = getAllFromDatabase('meetings');
+    res.status(200).send(meetings); 
+})
+
+meetingRouter.post('/', (req, res, next) => {
+    try {
+        const { time, date, day, note } = req.body || {};
+        const newMeeting = { time, date, day, note }
+        const created = addToDatabase('meetings', newMeeting);
+        res.status(201).send(created);
+    } catch(err) {
+        err.status = 400;
+        return next(err);
+    }
+});
+
+meetingRouter.delete('/:meetingId', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('meetings', req.params.meetingId)
+
+    if (deleted) {
+        res.status(204).send();
+    } else {
+        res.status(404).send('Meeting not found');
+    }
+})
+
+module.exports = meetingRouter;
